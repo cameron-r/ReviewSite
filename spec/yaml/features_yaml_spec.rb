c5ac5f51c83eec9eefcd6f5ac717fdecef40ebed
Cameron Rowshanbin <crowshan@thoughtworks.com>
2015-01-12 10:06:17 -0600

    Updated tests to make sure yaml file can be loaded correctly

--- spec/yaml/features_yaml_spec.rb
+++ spec/yaml/features_yaml_spec.rb
@@ -2,8 +2,8 @@ require 'yaml'
 
 describe('features yaml file') do
     context("there is a correctly formatted yaml file") do
-        it("should load correctly") do
-            feature_yaml = YAML.load_file('properties/features.yaml')
+        it("should not raise error") do
+            lambda { YAML.load_file('properties/features.yaml') }.should_not raise_error
         end
         
         it("should have a 'features' key") do
